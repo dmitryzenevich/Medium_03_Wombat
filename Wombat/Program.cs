6172a4b40443bf61d3e00f0b562b37a7116c6ff5
Dmitry <dmitryzenevich@gmail.com>
2020-08-22 00:50:42 +0300

    init

---
+++ Wombat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wombat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Wombat/Untitled.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Wombat
+{
+    class Wombat{
+        public int Health;
+        public int Armor;
+
+        public void TakeDamage(int damage){
+            Health -= damage - Armor;
+            if(Health <= 0){
+                Console.WriteLine("Я умер");
+            }
+        }
+    }
+
+    class Human{
+        public int Health;
+        public int Agility;
+
+        public void TakeDamage(int damage){
+            Health -= damage / Agility;
+            if(Health <= 0){
+                Console.WriteLine("Я умер");
+            }
+        }
+    }
+}
